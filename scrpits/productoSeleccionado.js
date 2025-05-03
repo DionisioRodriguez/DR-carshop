0a531160f5b70837bad717db33c45c17cd01796a
DionisioRodriguez <dionisio.joaquin.rodriguez@gmail.com>
2025-05-03 00:02:30 -0300

    adaptando el JS para que funcione con el nuevo JSON

--- scrpits/productoSeleccionado.js
+++ scrpits/productoSeleccionado.js
@@ -1,2 +1,10 @@
 
+const params = new URLSearchParams(window.location.search);
+const idProducto = params.get('id');
+const producto = Productos.find(p => p.id === idProducto);
 
+
+if (producto) {
+
+  document.getElementById('')
+}
