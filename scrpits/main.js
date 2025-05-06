348ac586a2c0491c0823c22cdb7d46147831cff0
DionisioRodriguez <dionisio.joaquin.rodriguez@gmail.com>
2025-05-06 18:17:08 -0300

    Agregando JS para mostrar vista del producto que intereasa al cliente

--- scrpits/main.js
+++ scrpits/main.js
@@ -1,9 +1,11 @@
 
+
 const contenedorProductos = document.querySelector('.productos-container');
 const botonesCategorias = document.querySelectorAll('.boton-categoria');
 const tituloPrincipal = document.querySelector('#titulo-principal');
 const parrafoPrincipal = document.querySelector('#parrafo-principal');
 const contenedorImagenPrincipal = document.querySelector('.productos-imagen--container');
+
 
 
 const dataUrl = "data/array.json";
@@ -24,6 +26,7 @@ fetch(dataUrl)
   })
   .finally(() => {
     cargarProductos(Productos);
+    ventanaProductoSeleccionado()
   });
 
 
@@ -36,18 +39,15 @@ function cargarProductos(productosElegidos) {
     const div = document.createElement('div');
     div.classList.add("producto-card");
     div.innerHTML = `
-      <a id="${producto.id}" class="Link-producto-seleccionado" href="/assets/productoSeleccionado.html">
-        <img class="producto-imagen" src="${producto.imagen}" alt="${producto.titulo}">
-        <div>
-          <h2>${producto.titulo}</h2>
-          <p><span>$${producto.precio},00</span></p>
-        </div>
-      </a>
+      <img class="producto-imagen" src="${producto.imagen}" alt="${producto.titulo}">
+      <div>
+        <h2>${producto.titulo}</h2>
+        <p><span>$${producto.precio},00</span></p>
+      </div>
+      <button class="ver-mas-button" id="${producto.id}">Ver más</button>
     `;
     contenedorProductos.append(div);
   })
-
-  actualizarProductosPantalla()
 
 }
 
@@ -80,15 +80,87 @@ botonesCategorias.forEach(boton => {
   })
 });
 
-function actualizarProductosPantalla() {
 
-  productosPantalla = document.querySelectorAll('.Link-producto-seleccionado')
+const contenedorProductoInfo = document.querySelector('.producto-seleccionado-info');
 
-  productosPantalla.forEach(boton => {
-    boton.addEventListener('click', mostrarProductoSeleccionado)
+function ventanaProductoSeleccionado() {
+
+  const btnVerMas = document.querySelectorAll('.ver-mas-button');
+
+  btnVerMas.forEach(boton => {
+    boton.addEventListener('click', () => {
+      contenedorProductoInfo.classList.toggle('hidden-producto-info')
+      cargarProducto(boton.id)
+    });
   })
 }
 
-function mostrarProductoSeleccionado (e) {
-  window.location.href = `/assets/productoSeleccionado.html?id=${e.currentTarget.id}`;
-}
+function cargarProducto(productoElegidoiId) {
+
+  contenedorProductoInfo.innerHTML = '';
+
+  producto = Productos.find(p => p.id === productoElegidoiId);
+
+  if (producto) {
+
+    if (producto.categoria.nombre === 'Shampoo') {
+      const div = document.createElement('div');
+    div.classList.add("producto-seleccionado--container");
+    div.innerHTML = `
+      <div class="producto-seleccionado-izquierdo">
+        <img src="${producto.imagen}" alt="${producto.titulo}">
+        <div>
+          <p>${producto.especificacion}</p>
+        </div>
+      </div>
+      <div class="producto-seleccionado-derecho">
+        <div class="producto-seleccionado-nombre-precio">
+          <h1>${producto.categoria.nombre} ${producto.titulo}</h1>
+          <span>$${producto.precio},00</span>
+        </div>
+        <div class="producto-seleccionado-cantidad">
+          <span>Cantidad</span>
+          <input value="1" type="number" min="1" max="15">
+        </div>
+        <div class="producto-seleccionado-button">
+          <a href="/assets/productos.html">
+            <button id="${producto.id}" class="boton-agregar">Agregar al carrito</button>
+          </a>
+        </div>
+      </div>
+    `
+
+    contenedorProductoInfo.append(div);
+
+    } else {
+      const div = document.createElement('div');
+    div.classList.add("producto-seleccionado--container");
+    div.innerHTML = `
+      <div class="producto-seleccionado-izquierdo">
+        <img src="${producto.imagen}" alt="${producto.titulo}">
+        <div>
+          <p>${producto.especificacion}</p>
+        </div>
+      </div>
+      <div class="producto-seleccionado-derecho">
+        <div class="producto-seleccionado-nombre-precio">
+          <h1>${producto.titulo}</h1>
+          <span>$${producto.precio},00</span>
+        </div>
+        <div class="producto-seleccionado-cantidad">
+          <span>Cantidad</span>
+          <input value="1" type="number" min="1" max="15">
+        </div>
+        <div class="producto-seleccionado-button">
+          <a href="/assets/productos.html">
+            <button id="${producto.id}" class="boton-agregar">Agregar al carrito</button>
+          </a>
+        </div>
+      </div>
+    `
+
+    contenedorProductoInfo.append(div);
+
+    }
+  }
+  }
